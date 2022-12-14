c448af45f146ab6b71b8ba6775ad8bfd660a775e
amihai <amihai.offenbacher@voyantis.ai>
2022-12-14 16:40:21 +0200

    added read\load yaml

--- src/my_utils/readers.py
+++ src/my_utils/readers.py
@@ -1,11 +1,22 @@
 import json
+import yaml
 
 
-def save_json(data, path, indent=4, ensure_ascii=False):
+def save_json(data: dict, path: str, indent=4, ensure_ascii=False, **kwargs):
     with open(path, 'w') as f:
-        json.dump(data, f, indent=indent, ensure_ascii=ensure_ascii)
+        json.dump(data, f, indent=indent, ensure_ascii=ensure_ascii, **kwargs)
 
 
-def read_json(path):
+def read_json(path: str, **kwargs):
     with open(path, 'r') as f:
-        return json.load(f)
+        return json.load(f, **kwargs)
+
+
+def read_yaml(path: str):
+    with open(path, 'r') as f:
+        return yaml.safe_load(f)
+
+
+def save_yaml(data: dict, path: str, **kwargs):
+    with open(path, 'w') as f:
+        yaml.dump(data, f, **kwargs)
