4378115e484eab2f802d2952bf7a01dee37735e9
amihai <amihai.offenbacher@voyantis.ai>
2022-12-14 16:43:30 +0200

    added reindent yaml

--- src/my_utils/readers.py
+++ src/my_utils/readers.py
@@ -20,3 +20,14 @@ def read_yaml(path: str):
 def save_yaml(data: dict, path: str, **kwargs):
     with open(path, 'w') as f:
         yaml.dump(data, f, **kwargs)
+
+
+def reindent_yaml(path: str):
+    """
+    sometimes yaml files are saved like json files and are not indented (one long line).
+    This function reindents the yaml file.
+    :param path:
+    :return:
+    """
+    yaml_data = read_json(path)
+    save_yaml(yaml_data, path)
